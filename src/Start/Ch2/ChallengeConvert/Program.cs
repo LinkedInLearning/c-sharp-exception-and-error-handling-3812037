be6474ee93784512ead29fe1e98f6ff54c66587c
Joe Marini <cjmarini68@gmail.com>
2024-02-01 21:45:22 +0000

    Nullable types

--- src/Start/Ch2/ChallengeConvert/Program.cs
+++ src/Start/Ch2/ChallengeConvert/Program.cs
@@ -5,13 +5,13 @@ bool Continue = true;
 
 while (Continue) {
     Console.WriteLine("Enter the first integer number: ");
-    string str = Console.ReadLine();
+    string? str = Console.ReadLine();
     Console.WriteLine("Enter the second integer number: ");
-    string str2 = Console.ReadLine();
+    string? str2 = Console.ReadLine();
 
     Console.WriteLine("Enter the operation: (a)dd, (s)ubtract, (m)ultiply, (d)ivide or 'exit' to quit: ");
-    string op = Console.ReadLine();
-    op = op.ToLower();
+    string? op = Console.ReadLine();
+    op = op?.ToLower();
     if (op == "exit") {
         break; // exit the loop at this point and terminate the program
     }
